c40883cb5348dda141fe7a5e4f07284293c89e05
superbDerrick <kang.derrick@gmail.com>
2021-03-08 14:24:46 +0900

    more update

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('polls.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    path('index/', include('polls.urls')),
 
 ]
